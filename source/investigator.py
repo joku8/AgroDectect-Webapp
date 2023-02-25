4c44c7c29dafb8f06780fe09aa71c1c9452bb2c2
joku8 <jsku2@illinois.edu>
2023-02-25 15:39:33 -0600

    investivator.py init

--- source/investigator.py
+++ source/investigator.py
@@ -1,6 +1,14 @@
+import requests
+import json
+
+API_ENDPOINT = 'https://api.nal.usda.gov/pubag/rest/search/'
+API_KEY = 'YOUR_API_KEY'
+
+
 
 
 # Function takes in keywords and returns top 5 research articles related to it
-def output_research(keywords):
+def output_pubag_research(keywords):
     # Code here
     print("not implemented yet")
+
--- source/main.py
+++ source/main.py
@@ -1,7 +1,13 @@
 from Utils import *
 from ml_Utils import *
+from investigator import *
 
 # create_model("corn")
 # create_model("soybean")
 
 # print(predict("images/corn_blight_test.jpeg", "corn"))
+# print(predict("images/definetelyNOTcommon_rust.jpeg", "corn"))
+# print(predict("images/healthy_test.jpeg", "corn"))
+
+KEYWORDS = ["corn", "common_rust", "yeild", "treatment"]
+output_research()
